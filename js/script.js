e181a23a95ef73ac176c44011e18cc18067c4ae5
Axhu99 <xhuliodreni@icloud.com>
2023-12-19 18:37:08 +0100

    base with layout

--- js/script.js
+++ js/script.js
@@ -11,28 +11,58 @@
     - fare un tasto annulla 
 */
 
-const button = document.getElementById('button');
+const buttonGo = document.getElementById('button-go');
+const buttonReturn = document.getElementById('button-return');
 const inputName = document.getElementById('user-name');
 const inputKms = document.querySelector('.kms');
 const inputAge = document.getElementById('select-age');
+const ticketUserame = document.querySelector('.ticket-username');
+const ticketDiscount = document.querySelector('.ticket-discount');
+const ticketCart = document.querySelector('.ticket-cart');
+const ticketNumber = document.querySelector('.ticket-number');
+const ticketPrice = document.querySelector('.ticket-price');
 const priceForKms = 0.21;
 let userName;
 let userKms;
+let randomCart;
+let randomNumber;
+const max = 999999;
+const min = 100000;
+let discount = 'Biglietto Standard'
 
-button.addEventListener('click', function () {
+buttonGo.addEventListener('click', function () {
     
     // prendiamo i dati inseriti dall'utente
     userName = inputName.value;
     userKms = parseInt(inputKms.value);
     const userAge = inputAge.value
 
+    // numero carrozza
+    randomCart = Math.floor(Math.random() * 15) + 1;
+
+    // numero biglietto
+    randomNumber = Math.floor(Math.random() * (max + 1 - min)) + min;
+
     // il prezzo del biglietto
     let realprice = userKms * priceForKms;
     
     if (userAge === 'minorenne'){
         realprice *= 0.8;
+        discount = 'Biglietto per minorenni'
     } else if (userAge === 'over'){
         realprice *= 0.6
+        discount = 'Biglietto per over 65'
     }
 
+    ticketUserame.innerText = userName;
+    ticketDiscount.innerText = discount;
+    ticketCart.innerText = randomCart;
+    ticketNumber.innerText = randomNumber;
+    ticketPrice.innerText = realprice;
+})
+
+buttonReturn.addEventListener('click', function (){
+    inputName.value = '';
+    inputKms.value = '';
+    inputAge.value = '';
 })
