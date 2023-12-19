e0633cd6dea700c78a924f8bd47ef7f26f1e6c78
Axhu99 <xhuliodreni@icloud.com>
2023-12-19 15:55:07 +0100

    lineup

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/* 
+    1- chiedere il nome
+    2- chiedere i km da fare 
+    3- selezionare una fascia di eta'
+    4- generare un biglietto con:
+        a- il nome
+        b- l'offerta(che ci sia o meno)
+        c- dati generati casualmente (tipo numero del biglietto)
+        d- costo del biglietto
+    BONUS
+    - fare un tasto annulla 
+*/
